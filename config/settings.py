2923900d3d9a71865a8f5d5f166a171a79b69d85
Jitian426 <userlistyoshida@gmail.com>
2025-02-12 17:23:27 +0900

    Fix: Add gunicorn and update ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-+xycu!ob5rt95&b9)mir_nol97y8qgc#qgcz330npwbjag+dgz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "your-app.onrender.com"]
 
 
 # Application definition
