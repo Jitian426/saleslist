4f74596814abae3344896c3032811f638e593bb1
Jitian426 <userlistyoshida@gmail.com>
2025-03-27 05:44:47 +0900

    会社詳細情報編集ログを記録

--- saleslist/models.py
+++ saleslist/models.py
@@ -77,3 +77,19 @@ class SalesPerson(AbstractUser):
     def __str__(self):
         return self.username
 
+
+from django.conf import settings
+from django.db import models
+
+class CompanyEditLog(models.Model):
+    company = models.ForeignKey("Company", on_delete=models.CASCADE, related_name="edit_logs")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    action = models.CharField(max_length=50)  # "新規登録" / "情報編集"
+    changed_fields = models.JSONField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"{self.timestamp} - {self.action} by {self.user}"
