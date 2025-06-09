b067cf79ce4ccb34d798c2f7c4a9f0dab810a4cc
Jitian426 <userlistyoshida@gmail.com>
2025-06-10 00:09:21 +0900

    url_params.py修正

--- saleslist/templatetags/url_params.py
+++ saleslist/templatetags/url_params.py
@@ -1,18 +1,21 @@
-# saleslist/templatetags/url_params.py
-
 from django import template
-from urllib.parse import urlencode
 
 register = template.Library()
 
 @register.filter
-def url_params(get_params, *args):
-    query_dict = get_params.copy()
+def url_params(current_params, *new_params):
+    """
+    現在のGETパラメータに新しいパラメータをマージしてURLクエリ文字列に変換する。
+    例: current_params='a=1&b=2' と new_params=['b=3', 'c=4'] を渡すと 'a=1&b=3&c=4'
+    """
+    from urllib.parse import parse_qs, urlencode
 
-    for param in args:
+    # 現在のクエリパラメータを辞書化
+    param_dict = {k: v[-1] for k, v in parse_qs(current_params).items()}
+
+    for param in new_params:
         if "=" in param:
             key, value = param.split("=", 1)
-            query_dict.pop(key, None)
-            query_dict[key] = value
+            param_dict[key] = value
 
-    return urlencode(query_dict)
+    return urlencode(param_dict)
