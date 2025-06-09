2da55af2fd18a94fc9ce59f0b18bbf75f98bbfee
Jitian426 <userlistyoshida@gmail.com>
2025-06-10 01:30:18 +0900

    url_params.pyとテンプレート修正

--- saleslist/templatetags/url_params.py
+++ saleslist/templatetags/url_params.py
@@ -1,21 +1,18 @@
+# saleslist/templatetags/url_params.py
 from django import template
+from urllib.parse import parse_qs, urlencode
 
 register = template.Library()
 
-@register.filter
-def url_params(current_params, *new_params):
+@register.simple_tag
+def url_params(request_get, **kwargs):
     """
-    現在のGETパラメータに新しいパラメータをマージしてURLクエリ文字列に変換する。
-    例: current_params='a=1&b=2' と new_params=['b=3', 'c=4'] を渡すと 'a=1&b=3&c=4'
+    現在のGETパラメータに任意のキーを上書き・追加してURLクエリ文字列を返す。
+    例: {% url_params request.GET sort='name' order='desc' %}
     """
-    from urllib.parse import parse_qs, urlencode
+    param_dict = {k: v[-1] for k, v in parse_qs(request_get.urlencode()).items()}
 
-    # 現在のクエリパラメータを辞書化
-    param_dict = {k: v[-1] for k, v in parse_qs(current_params).items()}
-
-    for param in new_params:
-        if "=" in param:
-            key, value = param.split("=", 1)
-            param_dict[key] = value
+    for key, value in kwargs.items():
+        param_dict[key] = value
 
     return urlencode(param_dict)
