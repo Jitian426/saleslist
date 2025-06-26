2723dcd52b7b42ef0c7185e5938204d5e9fdf3f3
Jitian426 <userlistyoshida@gmail.com>
2025-06-26 23:24:31 +0900

    軽微な会社情報ページ修正

---
+++ saleslist/templatetags/custom_filters.py
@@ -0,0 +1,18 @@
+# saleslist/templatetags/custom_filters.py
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def result_color_class(result):
+    if result in ['追わない', '不通留守', '担当不在', '失注']:
+        return 'btn-secondary'
+    elif result in ['見込', '再コール']:
+        return 'btn-success'
+    elif result == 'アポ成立':
+        return 'btn-warning'
+    elif result == '受注':
+        return 'btn-danger'
+    return 'btn-outline-primary'
+
